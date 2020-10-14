10c121bd7996da08fb3fda18a6376802c10008d1
Alexey Averkin <lavmint@gmail.com>
2020-10-14 23:01:07 +0400

    Initial Commit

---
+++ StockQuotes/StockQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockQuotesApp.swift
+//  StockQuotes
+//
+//  Created by Alexey Averkin on 14.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct StockQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
